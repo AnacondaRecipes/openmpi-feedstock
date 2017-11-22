539ce284f2e348ed97aaf6ad2befa95a61c393ea
Jiani niu <jianiniu@Jianis-Air.lan>
2017-11-21 21:24:31 -0800

    document the magic flags.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 if [[ $OSTYPE == darwin* ]]; then
+    # this may need to be done on linux too,
+    # see https://github.com/ContinuumIO/anaconda-issues/issues/739
     export LDFLAGS="$LDFLAGS -Wl,-rpath,${CONDA_PREFIX}/lib"
 fi
 
+# don't know if this is useful.
 export FCFLAGS="$FFLAGS"
 
 ./configure --prefix=$PREFIX \
