0d1d4bea34134200a427bdc72360dd13ce48088c
vchychy <vchychyrko@anaconda.com>
2025-09-30 11:23:32 +0300

    fix test mpicxx

--- recipe/tests/helloworld.cxx
+++ recipe/tests/helloworld.cxx
@@ -1,22 +1,28 @@
+
+// Openmpi since version =>5 and higher were removed C++ binding,
+// therefore, C++ testing code is using C binding.
+
 #include <mpi.h>
 #include <iostream>
+#include <vector>
 
-int main(int argc, char *argv[])
-{
-  MPI::Init();
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
 
-  int size = MPI::COMM_WORLD.Get_size();
-  int rank = MPI::COMM_WORLD.Get_rank();
-  int len; char name[MPI_MAX_PROCESSOR_NAME];
-  MPI::Get_processor_name(name, len);
+    int size = 0, rank = 0;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  std::cout <<
-    "Hello, World! " <<
-    "I am process "  << rank <<
-    " of "           << size <<
-    " on  "          << name <<
-    "."              << std::endl;
+    int name_len = 0;
+    char name[MPI_MAX_PROCESSOR_NAME];
+    MPI_Get_processor_name(name, &name_len);
 
-  MPI::Finalize();
-  return 0;
+    std::cout << "Hello, World! "
+              << "I am process " << rank
+              << " of " << size
+              << " on " << name
+              << "." << std::endl;
+
+    MPI_Finalize();
+    return 0;
 }
